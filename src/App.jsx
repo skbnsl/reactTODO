2b2486ba264cfabd77e7de81809c0aeaf86389c2
skbnsl <bansalsagar13@gmail.com>
2025-06-09 18:31:53 +0530

    working on todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './component/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
+  // const handleClick = () => {
+  //   console.log("button clicked");
+  // };
+
+  // const handleCounter = (count) => {
+  //   setCount(count+2);
+  // }
+
+  // return (
+  //   <>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => handleCounter(count)}> count is {count} </button>
+  //       <button onClick={handleClick}>  </button>
+  //     </div>
+
+  //     <input type="text" onMouseEnter={()=>console.log("text field enter")} />
+  //   </>
+  // )
+
+  const [todos, setTodos] = useState([]);
+  const [input,setInput] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <div><h2>TODO App!!</h2></div>
+      <Todo/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/component/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div> 
+        <h2>welcome from todo component!!</h2>
+    </div>
+  )
+}
+
+export default Todo
