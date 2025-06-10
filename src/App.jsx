235b80e77a1cd8c0dc159a58331ca5d2661c6b79
skbnsl <bansalsagar13@gmail.com>
2025-06-10 15:25:10 +0530

    todo working

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,70 @@ function App() {
   // )
 
   const [todos, setTodos] = useState([]);
-  const [input,setInput] = useState('');
+  const [inputValue,setInputValue] = useState('');
   const [editIndex, setEditIndex] = useState(null);
+
+  const handleAddTodo = () => {
+      if(inputValue.trim() == "") return;
+      if(editIndex!==null){
+        const updatedTodo = todos.map((todo,index) => 
+              index === editIndex ? {text:inputValue, completed:false} : todo);
+        setTodos(updatedTodo);
+        setEditIndex(null);
+      } else {
+         setTodos([...todos, {text:inputValue, completed:false}]);
+      }
+      setInputValue("");
+  }
+
+  const handleDeleteTodo = (index) => {
+    const updateTodos = todos.filter((_,i) => i != index);
+    setTodos(updateTodos);
+  }
+
+  const handleEditTodo = (index) => {
+    setInputValue(todos[index].text);
+    setEditIndex(index);
+  }
+
+  const toggleCompleted = (index) => {
+    const updatedTodo = todos.map((todo,i) => 
+      i===index ? {...todo, completed:!todo.completed} : todo
+    );
+    setTodos(updatedTodos);
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
   return (
     <>
-      <div><h2>TODO App!!</h2></div>
+      <div style={{padding : "20px"}}>
+        <h1>TODO App!!</h1>
+
+        <input type='text' value={inputValue} onChange={handleInputChange} 
+            placeholder='Add a Todo'/>
+
+        <button onClick={handleAddTodo}>
+            {editIndex === null ? "Add Todo" : "Update Todo"}
+        </button>
+
+      <ul>
+            {todos.map((todo,index) => (
+                <li key={index}>  
+                    {todo.text}
+                    <div>
+                      <button onClick={()=>handleEditTodo(index)}> Edit </button>
+                      <button onClick={()=>handleDeleteTodo(index)}> delete </button>
+                    </div>
+                 </li>
+            ))}
+      </ul>
+
+      
+
+      </div>
       <Todo/>
     </>
   )
